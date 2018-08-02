073aafcdb508fc2c750e0c91322e5cd491241810
rvences <rafael.vences@gmail.com>
2018-08-02 01:53:14 -0500

    Informacion Base

--- controllers/GeneralesusuariasController.php
+++ controllers/GeneralesusuariasController.php
@@ -106,7 +106,7 @@ class GeneralesusuariasController extends Controller
         $cedula = Cedulas::find()->where(['tipoatencion_id' => 2])->one();
         if ($cedula->tipoatencion_id == 2) {
             //$cedula_inicial = CedulasTelefonicas::find()->where(['cedula_id' => $cedula->id])->one();
-            $cedula_inicial = CedulasTelefonicas::find()->where(['cedula_id > 0' ])->one();
+            $cedula_inicial = CedulasTelefonicas::find()->one();
 
             $model->cedula_id = $cedula_inicial->cedula_id;
             $model->nombre = $cedula_inicial->nombre_temporal;
